663c9a10945a1b54fb8864504e327642aeb5ca19
Naomi-Rhames <naomirhames2020@gmail.com>
2022-03-23 22:27:10 -0500

    Setting up State,created component and container file including actioncreators

---
+++ src/redux/reducer.js
@@ -0,0 +1,35 @@
+const initialAffirmation ={
+    phrase: "",
+    id: null
+}
+
+const initialPost = {
+    message: "",
+    image_url: "",
+    id: null,
+    comments: []
+}
+
+const initialUser = {
+    email: "",
+    username: "",
+    bio: "",
+    id: null
+}
+
+const initialState = {
+   posts: [],
+   setAffirmations: initialAffirmation, 
+   setPost: initialPost,
+   user: initialUser 
+}
+
+export function reducer(state= initialState, action){
+    switch(action.type){
+
+
+        
+    default:
+        return {...state}
+    }
+}
